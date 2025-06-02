2aa2d4765cb67c8c34cfd5b1aef3d600b6aa1c75
Daniel Okumu <okumudaniel544@gmail.com>
2025-06-02 17:04:16 +0300

    first commit

---
+++ starter/script.js
@@ -0,0 +1,160 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+//api change
+//https://countries-api-836d.onrender.com/countries/
+
+///////////////////////////////////////
+
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`); //api endpoint
+
+//   request.send(); //asynchronous
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText); //refer to destructuring
+
+//     console.log(data);
+
+//     const html = `
+//         <article class="country">
+//           <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               data.population / 1000000
+//             ).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${
+//               data.currencies[0].name
+//             }</p>
+//           </div>
+//         </article>
+//   `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+
+//     countriesContainer.style.opacity = 1;
+//   }); //AJAX calls happen asynchronously. the AJAX call that has the first request respondse
+//   //... fires the load event first
+// };
+
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('germany');
+
+/////////////////////////////////////////////////////////////////
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v2/name/${country}`); //api endpoint
+
+  request.send(); //asynchronous
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText); //refer to destructuring
+
+    console.log(data);
+
+    //render country 1
+    renderCountry(data);
+
+    //get neighbour country
+    const neighbour = data.borders?.[0];
+
+    if (!neighbour) {
+      return;
+    } //guard clause if country 1 has no neighbour
+
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`); //api endpoint
+
+    request2.send(); //asynchronous
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  }); //AJAX calls happen asynchronously. the AJAX call that has the first request respondse
+  //... fires the load event first
+};
+
+// getCountryAndNeighbour('portugal');
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 second passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       },1000);
+//     },1000);
+//   },1000);
+// },1000);
+//deep nesting : hard to maintain and difficult to understand and reason about hence more bugs , and difficult to add more features and functionality
+
+//Promises and the fetch API************************************************************************************
+// const fetchRequest = fetch(`https://restcountries.com/v2/name/portugal`);
+
+// console.log(fetchRequest);//pending
+
+// setTimeout(() => {
+//   console.log(fetchRequest);//fullfilled after 3 seconds
+// }, 3000);
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json(); //json also returns a new promise
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+};
+
+getCountryData('madagascar');
+
+/////////////////////////////////////////////////
+//TCP - breaks requests/responses into small chunks called packets.
+// ... Than reassemble these packets once they have arrived to their location
+
+//IP - Sends/ Routes the packets through the internet using IP addresses on each packet
+
+//TCP disassembles and assembles the packets to and from the requests.
+// ... While IP determines how the packets are routed to their destination?

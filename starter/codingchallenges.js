bda22b0ff9d95d43f6d11193fce430c2c2c67c0c
Daniel Okumu <okumudaniel544@gmail.com>
2025-06-07 20:01:07 +0300

    consuming promises with async/await

--- starter/codingchallenges.js
+++ starter/codingchallenges.js
@@ -1,28 +1,76 @@
 //Coding Challenge 1************************************
 //this code is dependent on code from script.js and style.css
-const WhereAmI = function (lat, lng) {
-  fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status}`);
-      }
+// const WhereAmI = function (lat, lng) {
+//   fetch(
+//     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+//   )
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`${response.status}`);
+//       }
 
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data);
 
-      console.log(`Your are in ${data.city}, ${data.countryName}.`);
+//       console.log(`Your are in ${data.city}, ${data.countryName}.`);
 
-      getCountryData(data.countryName); //from script.js
-    })
-    .catch(error => console.error(`${error}. Unable to load resources`));
+//       getCountryData(data.countryName); //from script.js
+//     })
+//     .catch(error => console.error(`${error}. Unable to load resources`));
 
-  //
+//   //
+// };
+
+// WhereAmI(-20.6252, 46.24731);
+// WhereAmI(52.508, 13.381);
+// WhereAmI(19.037, 72.873);
+
+//Coding challenge 2*****************************************
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
 };
 
-WhereAmI(-20.6252, 46.24731);
-WhereAmI(52.508, 13.381);
-WhereAmI(19.037, 72.873);
+const imageContainer = document.querySelector('.images');
+
+createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const image = document.createElement('img');
+    image.src = imgPath;
+
+    image.addEventListener('load', function () {
+      imageContainer.append(image);
+      resolve(image);
+    });
+
+    image.addEventListener('error', function () {
+      reject(new Error(`Image not found`));
+    });
+  });
+};
+
+let currentImage;
+
+createImage('img/img-1.jpg')
+  .then(response => {
+    currentImage = response;
+    console.log(`Image 1 loaded`);
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(response => {
+    currentImage = response;
+    console.log(`Image 2 loaded`);
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+  })
+  .catch(error => console.error(error));
+// starter\img\img-1.jpg
--- starter/script.js
+++ starter/script.js
@@ -21,12 +21,12 @@ const renderCountry = function (data, className = '') {
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
 
-  // countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1; //comment to enable coding challenge 1 to work
 };
 
 const renderError = function (message) {
   countriesContainer.insertAdjacentText('beforeend', message);
-  // countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1; //comment to enable coding challenge 1 to work
 };
 
 //api change
@@ -254,15 +254,243 @@ btn.addEventListener('click', function () {
 });
 
 ////////////////////////////////////////////////////////////
-//The event loop in practice*****************************
+//The event loop in practice******************************************
 //microtask queue has priority over callback queue
-//working with timers and promises together may delay timers since promises have a priority in the microtask queue 
+//working with timers and promises together may delay timers since promises have a priority in the microtask queue
 //... over timers in the callback queue
-console.log('Test start');
-setTimeout(() => console.log(`0 seconds`), 0);
-Promise.resolve('Resolved Promise 1').then(response => console.log(response));
-Promise.resolve('Resolved Promise 2').then(response => {
-  for (let x = 0; x < 1000000; x++) {}
-  console.log(response);
+
+// console.log('Test start');
+// setTimeout(() => console.log(`0 seconds`), 0);
+// Promise.resolve('Resolved Promise 1').then(response => console.log(response));
+// Promise.resolve('Resolved Promise 2').then(response => {
+//   for (let x = 0; x < 100000; x++) {}
+//   console.log(response);
+// });
+// console.log(`Test end`);
+
+//Building a simple promise*********************************************
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log(`Lottery draw is happening`);
+
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You win 💰'); //becomes response in then()
+//     } else {
+//       reject(new Error('You lost...')); //catched as an error in catch()
+//     }
+//   }, 2000);
+// });
+
+// lotteryPromise
+//   .then(response => console.log(response))
+//   .catch(error => console.error(error));
+
+//Promisifying : converting callback based asynchronous behavior into promise based asynchronous behavior
+
+//Promisifying setTimeout()
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(1)
+//   .then(() => {
+//     console.log(`Waited 1 seconds`);
+
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log(`Waited 2 seconds`);
+
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log(`Waited 3 seconds`);
+
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log(`Waited 4 seconds`);
+//   });
+
+//reference
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 second passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       },1000);
+//     },1000);
+//   },1000);
+// },1000);
+
+//fulfilling and rejecting promises immediately
+// Promise.resolve('Resolved').then(response => {
+//   console.log(response);
+// });
+// Promise.reject(new Error('Problem')).catch(error => {
+//   console.error(error);
+// });
+
+//Promisifying the geolocation API*************************************************************
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // return navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   error => reject(error)
+    // );
+
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then(response => {
+  // console.log(response.coords);
 });
-console.log(`Test end`);
+
+const WhereAmI = function () {
+  // let lat, lng;
+  getPosition()
+    .then(response => {
+      const { latitude: lat, longitude: lng } = response.coords; //refer to destructuring
+
+      return fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+      );
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      // console.log(data);
+
+      // console.log(`Your are in ${data.city}, ${data.countryName}.`);
+
+      getCountryData(data.countryName);
+    })
+    .catch(error => console.error(`${error}. Unable to load resources`));
+
+  //
+};
+
+// WhereAmI();
+
+//Consuming promises with async/await*******************************************************
+//Error handling with try/catch*******************************************************
+//async turns a regular function into an asynchronous function
+
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+const WhereAmIasync = async function () {
+  try {
+    //geolocation
+    const position = await getPosition();
+    const { latitude: lat, longitude: lng } = position.coords;
+
+    //reverse geocoding
+    const geocodeResponse = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+    );
+
+    if (!geocodeResponse.ok) {
+      throw new Error('Problem getting location data');
+    }
+
+    const geocodeData = await geocodeResponse.json();
+
+    const response = await fetch(
+      `https://restcountries.com/v2/name/${geocodeData.countryName}`
+    );
+
+    if (!response.ok) {
+      console.log(response.status);
+      throw new Error('Problem getting country');
+    }
+
+    const data = await response.json();
+
+    renderCountry(data[0]);
+
+    return `You are in ${geocodeData.city}, ${geocodeData.countryName}`;
+  } catch (error) {
+    console.error(`${error}`);
+    renderError(error.message);
+
+    //rethrowing error to reject promise reuturned from async function**
+    throw error;
+  }
+};
+
+// console.log('Will get location');
+// console.log(WhereAmIasync());
+// WhereAmIasync()
+//   .then(returnedValue => {
+//     console.log(`2 : ${returnedValue}`);
+//   })
+//   .catch(error => {
+//     console.error(`${error}`);
+//   })
+//   .finally(() => {
+//     console.log('Finished getting location');
+//   });
+
+// (async function () {
+//   try {
+//     const returnedValue = await WhereAmIasync();
+//     console.log(`2 : ${returnedValue}`);
+//   } catch (error) {
+//     console.error(`${error}`);
+//   }
+//   console.log('Finished getting location');
+// })();
+
+// try {
+//   let x = 2;
+//   const y = 3;
+//   x = 6;
+// } catch (error) {
+//   alert(error.message);
+// }
+
+//Running Promises in parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    //the data is fetched in sequence : one after another, uses more time
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]); //takes in an array of promises and returns them at the same time
+
+    //note : when one promise rejects, the whole promise.all() rejects
+
+    console.log(data);
+
+    console.log(data.map(array => array[0].capital));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
